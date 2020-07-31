72de7297d714524c4ebd2f5896ddb64f89a25f69
yunyidi-han <69004029+yunyidi-han@users.noreply.github.com>
2020-07-31 14:00:59 -0700

    location map

--- com.TicTacToe.Board/PlayBoard.java
+++ com.TicTacToe.Board/PlayBoard.java
@@ -1,7 +1,25 @@
 package com.TicTacToe.Board;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PlayBoard {
     public StringBuilder board = new StringBuilder();
+    private static Map<Integer, Integer> locationMap = new HashMap<>();
+
+    static {locationMap = Map.of(11,14,
+            12,18,
+            13,22,
+            21, 33,
+            22,37,
+            23,41,
+            31,54,
+            32,58,
+            33,62);
+
+    }
+
+
     public StringBuilder buildBoard() {
         board.append("-- Board --\n\n");
         board.append("   |   |   \n");
